f3ecbad4fdc36f546b27926eed7fd40e25e7f036
yang yu <yuyang8986@hotmail.com>
2019-12-09 17:12:40 +1100

    Add project files.

---
+++ Droid/CustomUrlSchemeInterceptorActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+
+namespace OAuthNativeFlow.Droid
+{
+    [Activity(Label = "CustomUrlSchemeInterceptorActivity", NoHistory = true, LaunchMode = LaunchMode.SingleTop)]
+    [IntentFilter(
+        new[] { Intent.ActionView },
+        Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
+        DataSchemes = new[] { "com.googleusercontent.apps.476537655226-28e82e5nn5vjlv28bhrmendtfj8ek9bi" },
+        DataPath = "/oauth2redirect")]
+    public class CustomUrlSchemeInterceptorActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Convert Android.Net.Url to Uri
+            var uri = new Uri(Intent.Data.ToString());
+
+            // Load redirectUrl page
+            AuthenticationState.Authenticator.OnPageLoading(uri);
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.SetFlags(ActivityFlags.ClearTop | ActivityFlags.SingleTop);
+            StartActivity(intent);
+
+            this.Finish();
+
+            return;
+        }
+    }
+}
